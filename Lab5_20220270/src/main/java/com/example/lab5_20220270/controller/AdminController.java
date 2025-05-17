f68f6c294e7537ddda6d88ddb20c5683384d45bb
Christian Flores <167637198+Cjfs2005@users.noreply.github.com>
2025-05-16 21:56:53 -0500

    Avance 2

--- Lab5_20220270/src/main/java/com/example/lab5_20220270/controller/AdminController.java
+++ Lab5_20220270/src/main/java/com/example/lab5_20220270/controller/AdminController.java
@@ -1,4 +1,15 @@
 package com.example.lab5_20220270.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
 public class AdminController {
+    @GetMapping(value = {"", "/"})
+    public String listarEmpleados(Model model) {
+        return "admin/inicio";
+    }
 }
--- Lab5_20220270/src/main/java/com/example/lab5_20220270/controller/UserController.java
+++ Lab5_20220270/src/main/java/com/example/lab5_20220270/controller/UserController.java
@@ -1,4 +1,15 @@
 package com.example.lab5_20220270.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
 public class UserController {
+    @GetMapping(value = {"", "/"})
+    public String inicioUser(Model model) {
+        return "user/inicio";
+    }
 }
